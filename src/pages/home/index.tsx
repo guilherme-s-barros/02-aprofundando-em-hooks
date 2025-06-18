c68e84eaced526a71a40087918af0dfdf85d5c26
Guilherme <guilherme.s.barros@outlook.com>
2025-06-18 18:19:45 -0300

    Páginas e rotas - Aprimorando os inputs

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -21,13 +21,24 @@ export function Home() {
 						type="text"
 						id="task"
 						placeholder="Dê um nome para o seu projeto"
+						list="taskSuggestions"
 					/>
+
+					<datalist id="taskSuggestions">
+						<option value="Projeto 1" />
+						<option value="Projeto 2" />
+						<option value="Projeto 3" />
+						<option value="Projeto 4" />
+					</datalist>
 
 					<label htmlFor="minutesAmount">durante</label>
 					<MinutesAmountInput
 						type="number"
 						id="minutesAmount"
 						placeholder="00"
+						min={5}
+						max={60}
+						step={5}
 					/>
 
 					<span>minutos.</span>
